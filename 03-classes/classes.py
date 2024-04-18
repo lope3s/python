14d91aef1572bec744b51fdb69d4e978d2133cb1
Luan Lopes <luan.lopes@clickativo.com.br>
2024-04-18 11:37:14 -0300

    feature: adding notes about classes

---
+++ 03-classes/classes.py
@@ -0,0 +1,101 @@
+# class definition:
+class MyClass:
+    i = 12345
+
+    # for methods, the instance object is the first argument of the function.
+    def f(self):
+        return "hello world"
+
+# class instantiation:
+x = MyClass()
+
+# class definition with customizable instantiation:
+# when a class defines a __init__() method, class instantiation automatically invokes __init__() for
+# the newly created class instance.
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+x = Complex(3.0, -4.5)
+(x.r, x.i)
+
+
+class Dog:
+    king = 'canine' # class variable, shared by all instances
+
+    tricks = [] # shouldn't use list or dictionaries as class variables.
+
+    def __init__(self, name):
+        self.name = name # instance variable, unique to each instance
+
+    # calls to this method would update tricks property, which can cause unexpected behaviour in other instances of the class.
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+# if the same attribute name occurs in both an instance and a class, then attribute lookup prioritizes the instance.
+class Cat:
+    species = 'cat'
+
+    def __init__(self, species):
+        self.species = species
+
+# in python there's no enforcing of data hiding. It's all based upon conventions.
+# each value is an object, and therefore has a class (also called its type). It's stored as object.__class__.
+
+# inheritance
+
+class Animal:
+    def __init__(self, group):
+        self.group = group
+
+class Lion(Animal):
+    def __init__(self, name):
+        super()
+        self.name = name
+
+# An overriding method in a derived class may want to extend rather than simply replace the base class method.
+# A simple way to call the base class method directly is to call BaseClassName.methodname(self, arguments).
+# Clients can use this as well.
+
+# python has two built-in functions that work with inheritance:
+# isistance(obj1, obj2), will yield True only if obj.__class__ is equal to obj2 or some derived class from obj2.
+# issubclass(type1, type2), will check class inheritance.
+
+# multiple inheritance
+class Wolf(Animal, Dog):
+    def __init__(self, age):
+        self.age = age
+
+# You can think of the search for attributes inherited from a parent class as depth-first, left-to-right, not searching twice in the same class
+# where there is an overlap in the hierarchy. Thus if an attribute is not found in Wolf, it's searched for in Animal, then recursively in the base
+# class of Animal, and then it goes to class Dog and so on.
+
+# Private variables:
+# since there's no variable hiding in Python a convention followed by most Pyhon code is that a name prefixed with an underscore (e.g. _spam) should 
+# be treated as a non-public part of the API.
+
+# name mangling:
+# any identifier of the form __spam (at least two leading underscores, at most one trailing underscore) is textually replaced with _classname__spam,
+# where classname is the current class name with leading underscore(s) stripped.
+
+class Mapping:
+    def __init__(self, iterable):
+        self.items_list = []
+        self.__update(iterable)
+
+    def update(self, iterable):
+        for item in iterable:
+            self.items_list.append(item)
+
+    __update = update # private copy of original update() method.
+
+class MappingSubclass(Mapping):
+    def update(self, keys, values):
+        # provides new signature for update()
+        # but does not break __init__()
+        for item in zip(keys, values):
+            self.items_list.append(item)
+
+# The above example would work even if MappingSubclass were to introduce a __update identifier since it is replaced with _Mapping__update in the Mapping 
+# class and _MappingSubclass__update in the MappingSubclass class respectively.
